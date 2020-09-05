36ec6ae414217480e87b062bed361a81e3013f60
James Cosgrove <jamescosgrove2000@live.co.uk>
2020-09-05 20:28:56 +0100

    Working with 6 figures

--- SlotMachine/ContentView.swift
+++ SlotMachine/ContentView.swift
@@ -7,10 +7,87 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    @State private var score = 58
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        SlotMachineView(score: $score)
+    }
+}
+
+struct SlotMachineView: View {
+    @Binding var score: Int
+    var digits: [Int] {
+        var digitsArray = [0, 0, 0, 0, 0, 0]
+        let numberArray = String(score).compactMap{ $0.wholeNumberValue }
+        digitsArray.insert(contentsOf: numberArray, at: digitsArray.count - numberArray.count)
+        print(digitsArray)
+        return digitsArray
+    }
+    var body: some View {
+        VStack {
+            HStack {
+                ForEach((String(score).compactMap{ $0.wholeNumberValue }), id: \.self) { digit in
+                }
+                NumberView(number: digits[0])
+                NumberView(number: digits[1])
+                NumberView(number: digits[2])
+                NumberView(number: digits[3])
+                NumberView(number: digits[4])
+                NumberView(number: digits[5])
+            }
+            
+            Button(action: {
+                //withAnimation(.easeInOut(duration: 2)) {
+                score = Int.random(in: 0...999)
+                //}
+
+            }, label: {
+                Text("New Number: \(score)")
+            })
+        }
+
+    }
+}
+
+struct NumberView: View {
+    var number: Int = 0
+    var body: some View {
+        VStack {
+            NumberListView(number: number)
+                .offset(x: 0, y: CGFloat(135 - (30 * number)))
+                .animation(.easeInOut(duration: 2))
+                
+        }
+        .overlay(
+            Rectangle()
+                .stroke(Color.gray, lineWidth: 3)
+                .frame(width: 28, height: 34, alignment: .center)
+                //.fill(Color.clear)
+                
+        )
+        .mask(
+            Rectangle()
+                .fill(Color.green)
+                .frame(width: 30, height: 36, alignment: .center)
+                
+        )
+    }
+}
+
+struct NumberListView: View {
+    var number: Int = 0
+    var body: some View {
+        VStack {
+            ForEach(0...9, id: \.self) { number in
+                Text("\(number)")
+                    .font(Font.system(.largeTitle, design: .monospaced))
+                    .fontWeight(.medium)
+                    .frame(width: 20, height: 30, alignment: .center)
+                    
+            }
+        }
     }
 }
 
