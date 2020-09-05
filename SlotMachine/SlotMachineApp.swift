45da5b01f290856cdd115c792254934acf10b0e3
James Cosgrove <jamescosgrove2000@live.co.uk>
2020-09-05 18:27:07 +0100

    Initial Commit

---
+++ SlotMachine/SlotMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotMachineApp.swift
+//  SlotMachine
+//
+//  Created by James Cosgrove on 05/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct SlotMachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
